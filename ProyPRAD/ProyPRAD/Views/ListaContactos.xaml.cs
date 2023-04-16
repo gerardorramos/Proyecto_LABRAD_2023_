88062f79e44f0ceeef8af8aaf8d70bc2ba354010
Gerardo <Gerardo@PCGDRR>
2023-04-16 08:07:47 -0600

    Agregar archivos de proyecto.

---
+++ ProyPRAD/ProyPRAD/Views/ListaContactos.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyPRAD.Controllers;
+using ProyPRAD.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using ProyPRAD.Models;
+
+
+namespace ProyPRAD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaContactos : ContentPage
+    {
+
+        
+        string Number;
+        
+        double telefono;
+        
+        
+
+        public ListaContactos()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listacontactos.ItemsSource = await DateBase.ObtenerListaContactos();
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageUpdContact());
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageDelContact());
+        }
+
+        private async void listacontactos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            {
+                Models.Contacts Contacto = (Models.Contacts)e.CurrentSelection.FirstOrDefault();
+                Number = Contacto.Telefono.ToString();
+                
+                telefono = Contacto.Telefono;
+                
+            }
+
+            var Answer = await Application.Current.MainPage.DisplayAlert("Informacion", "Desea llamar al Contacto?", "Si", "No");
+            if (Answer == true)
+            {
+                try
+                {
+                    PlacePhoneCall(Number);
+
+                }
+                catch
+                 (System.Exception)
+                {
+                    _ = DisplayAlert("No se puede hacer llamada", "intente de nuevo", "ok");
+                }
+            }
+            else
+
+            {
+                return;
+
+            }
+
+        }
+
+        private void btnllamar_Clikced(object sender, EventArgs e)
+        {
+            PlacePhoneCall(Number);
+        }
+
+        public void PlacePhoneCall(string number)
+        {
+            try
+            {
+                PhoneDialer.Open(number);
+            }
+            catch (ArgumentNullException anEx)
+            {
+                
+                DisplayAlert("Advertencia", Convert.ToString(anEx), "Ok");
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                
+                DisplayAlert("Advertencia", Convert.ToString(ex), "Ok");
+            }
+            catch (Exception ex)
+            {
+                
+                DisplayAlert("Advertencia", Convert.ToString(ex), "Ok");
+            }
+        }
+
+    }
+}
---
+++ ProyPRAD/ProyPRAD/Views/ListaSitios.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyPRAD.Controllers;
+using ProyPRAD.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyPRAD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaSitios : ContentPage
+    {
+        ///variables para ver el sitio del mapa
+        public int id;
+        public double lat;
+        public double lon;
+        public string pais;
+
+        public ListaSitios()
+        {
+            InitializeComponent();
+        }
+
+        private async void listasitios_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var Answer = await Application.Current.MainPage.DisplayAlert("Informacion", "Desea ver la ubicación?", "Si", "No");
+            if (e.CurrentSelection != null)
+
+                if (Answer == true)
+                {
+                    {
+                        var Site = new Sites()
+                        {
+                            Id = id,
+                            Longitud = lon,
+                            Latitud = lat,
+                            Pais = pais
+
+                        };
+
+                        Models.Sites sitio = (Models.Sites)e.CurrentSelection.FirstOrDefault();
+
+                        var mappage = new Views.PageMaps();
+                        mappage.BindingContext = sitio;
+                        await Navigation.PushAsync(mappage);
+                    }
+                }
+        }
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            listasitios.ItemsSource = await DateBase.ObtenerListaSitios();
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageUpdSites());
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageDelSites());
+        }
+
+        private async void btnvermapa_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageMaps());
+        }
+    }
+}
